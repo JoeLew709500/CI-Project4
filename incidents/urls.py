cb6f4bdac8fcabb5a8fe838ee355bf3ef6b1d5b8
Joe Lewis <walesrugby709500@gmail.com>
2024-02-10 21:40:25 +0000

    add create new incident

--- incidents/urls.py
+++ incidents/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.incident_list, name='incident_list'),
     path('<int:incident_id>/', views.incident_detail, name='incident_detail'),
-]
+    path('incident/new/', views.incident_new, name='incident_new'),
+]       
