dea86b83a0143e583e5d9ec060decfa0928a3844
Joe Lewis <walesrugby709500@gmail.com>
2024-02-11 13:09:24 +0000

    Add date and time pickers

--- incidents/forms.py
+++ incidents/forms.py
@@ -4,14 +4,27 @@ from django import forms
 class IncidentForm(forms.ModelForm):
     class Meta:
         model = Incident
-        fields = ['location', 'incident_category', 'received_on', 'details','closed_on']
+        fields = ['location', 'incident_category','received_on', 'details','closed_on']
+
+        widgets = {
+            'received_on': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'closed_on': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
 
 class ActionForm(forms.ModelForm):
     class Meta:
         model = Action
         fields = ['incident', 'action_code', 'details', 'completed_on']
 
+        widgets = {
+            'completed_on': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+
 class ActionFormNew(forms.ModelForm):
     class Meta:
         model = Action
         fields = ['action_code', 'details', 'completed_on']
+
+        widgets = {
+            'completed_on': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
