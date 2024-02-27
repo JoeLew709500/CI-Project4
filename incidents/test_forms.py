5ef859804b11c0c68d4ff9a30b14cd453bebfd0a
Joe Lewis <walesrugby709500@gmail.com>
2024-02-27 18:54:25 +0000

    Add: doc strings

--- incidents/test_forms.py
+++ incidents/test_forms.py
@@ -8,6 +8,9 @@ class TestIncidentForm(TestCase):
     Test Incident Form
     """
     def test_incident_form(self):
+        """
+        Test Incident Form Valid
+        """
         form = IncidentForm({
             'location': 'Test Location',
             'incident_category': 1,
@@ -15,8 +18,10 @@ class TestIncidentForm(TestCase):
             'details': 'Test Details',
         })
         self.assertTrue(form.is_valid(), msg=form.errors)
-    
     def test_incident_form_invalid(self):
+        """
+        Test incident with invalid data
+        """
         form = IncidentForm({
             'location': True,
             'incident_category': 'p',
@@ -30,6 +35,9 @@ class TestActionForm(TestCase):
     Test Action Form
     """
     def setUp(self):
+        """
+        Set up user and incident
+        """
         self.user = User.objects.create_user(
             username='testuser',
             password='testpassword'
@@ -44,6 +52,9 @@ class TestActionForm(TestCase):
         )
 
     def test_action_form(self):
+        """
+        Test Action Form Valid
+        """
         form = ActionForm({
             'incident': 1,
             'action_code': 1,
@@ -52,6 +63,9 @@ class TestActionForm(TestCase):
         self.assertTrue(form.is_valid(), msg=form.errors)
 
     def test_action_form_date(self):
+        """
+        Test Action Form Valid with date
+        """
         form = ActionForm({
             'incident': 1,
             'action_code': 1,
@@ -59,8 +73,10 @@ class TestActionForm(TestCase):
             'completed_on': '2021-01-01T00:00',
         })
         self.assertTrue(form.is_valid(), msg=form.errors)
-    
     def test_action_form_invalid(self):
+        """
+        Test action with invalid data
+        """
         form = ActionForm({
             'incident': 1,
             'action_code': 'q',
@@ -68,4 +84,3 @@ class TestActionForm(TestCase):
         })
         self.assertFalse(form.is_valid(), msg=form.errors)
         self.assertEqual(form.errors['details'], ['This field is required.'])
-
