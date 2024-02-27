06ff43a675324daf846212dc10b8e6a0310a51e9
Joe Lewis <walesrugby709500@gmail.com>
2024-02-27 19:48:09 +0000

    Fix: pep8 compliance

--- incidents/test_views.py
+++ incidents/test_views.py
@@ -1,11 +1,16 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
-from .models import Incident, Action, ActionPhoto
-from .forms import IncidentForm, ActionForm, ActionFormNew, IncidentFormSearch
+from .models import Incident, Action
 
 class TestIncidentViews(TestCase):
-    def setUp(self):
+    """
+    Test the views for the incidents app
+    """
+    def set_up(self):
+        """
+        Set up user,incident and action
+        """
         self.user = User.objects.create_superuser(
             username='testuser',
             password='testpassword',
@@ -29,31 +34,49 @@ class TestIncidentViews(TestCase):
         )
 
     def test_index_view(self):
+        """
+        Test the index view
+        """
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'incidents/index.html')
 
     def test_incident_list_view(self):
+        """
+        Test the incident list view
+        """
         response = self.client.get(reverse('incident_list'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'incidents/incident_list.html')
 
     def test_incident_detail_view(self):
+        """
+        Test the incident detail view
+        """
         response = self.client.get(reverse('incident_detail', args=[self.incident.id]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'incidents/incident.html')
 
     def test_incident_create_view(self):
+        """
+        Test the incident create view
+        """
         response = self.client.get(reverse('incident_new'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'incidents/incident.html')
 
     def test_action_create_view(self):
+        """
+        Test the action create view
+        """
         response = self.client.get(reverse('action_new', args=[self.incident.id]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'incidents/action_detail.html')
 
     def test_photos_view(self):
+        """
+        Test the photos view
+        """
         response = self.client.get(reverse('photos', args=[self.incident.id, self.action.id]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'incidents/action_photos.html')
